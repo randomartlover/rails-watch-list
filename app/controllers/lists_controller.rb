18f12a1f18139d3a5aa409f0f1a772a932cb65e9
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-11-11 13:33:46 +0000

    viewing list controller and html done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
   def index
     @lists = List.all
   end
@@ -16,7 +17,13 @@ class ListsController < ApplicationController
     end
   end
 
+  def show; end
+
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
